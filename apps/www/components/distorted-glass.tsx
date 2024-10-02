46bd94fe30c7577d7f28a828ef9cb8e1f183fcf8
shaikhmubin02 <shaikhmubin572@gmail.com>
2024-10-02 18:02:24 +0530

    init

--- apps/www/components/distorted-glass.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export const DistortedGlass = () => {
-  return (
-    <div className="relative h-[50px] w-[360px] overflow-hidden rounded-b-2xl lg:w-[600px] xl:w-full">
-      <div className="absolute inset-0 backdrop-blur-md rounded-b-2xl" />
-    </div>
-  )
-}
